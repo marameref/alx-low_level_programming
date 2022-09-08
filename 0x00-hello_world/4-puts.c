1a8102275568034e072e903abeff2414f1740aba
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-09-08 10:48:25 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ *  *main - Entry point
+ *
+ *   *Return: Always 0 (Success)
+ *
+ *    */
+
+int main(void)
+{
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}
+
