423b7406eacc0b050f9500f2c76f994f076c90f3
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-09-22 03:49:42 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: Add one line to this code, so that the program prints a[2] = 98
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
