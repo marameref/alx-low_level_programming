2a00b1421d08826d7e50a62a81e31d1e299785ef
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-09-28 15:33:38 +0100

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion --> function that prints a string recursively
+* @s: string parameter
+*/
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
