b6d8d298602425f469859cfb1f937e92d3c593ec
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-09-28 16:03:47 +0100

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that return the value of x raised
+ * to the power of y
+ * @x: first parameter
+ * @y: second parameter
+ * Return: x power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
