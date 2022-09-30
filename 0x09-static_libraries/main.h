11f97f1ae22aef630a108f901a93f98f16fe4b0f
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-09-30 20:53:17 +0100

    Updated main

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
