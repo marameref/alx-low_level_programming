a07091ad7ad51ee8a86db71480d92743039ad328
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-09-30 21:06:47 +0100

    Static Lib

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar -rc liball.a *.o
+gcc -c *.c
+ar -rcs liball.a *.o
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
