8c84447d4dd1a70480121363a6b45f0340445a4d
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-12-12 15:07:54 -0800

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-LD_PRELOAD=/$PWD/libtest.so ./gm 9 8 10 24 75 9
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
