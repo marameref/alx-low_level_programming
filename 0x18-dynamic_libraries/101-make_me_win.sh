9ff2bf1d6170290cace26379bdad2f885eed33ae
Amarachi Omereife <mirrorprojectsltd@gmail.com>
2022-12-12 14:27:05 -0800

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://github.com/marameref/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.o
+export LD_PRELOAD=/tmp/libtest.so
